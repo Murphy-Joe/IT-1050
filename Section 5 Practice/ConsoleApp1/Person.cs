42ce501c99668d3d5cce3a03d9df890548b1ab9c
Murphy-Joe <ibidnsell@yahoo.com>
2020-04-07 15:51:23 -0400

    adding section 5 practice to github. might make a mess

---
+++ Section 5 Practice/ConsoleApp1/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+        private string _firstName;
+     
+        public string GetFirstName()
+        {
+            return this._firstName;
+        }
+
+        public void SetFirstName()
+        {
+            Console.WriteLine("Type the first name: ");
+            this._firstName = Console.ReadLine();
+        }
+
+        private string _lastName;
+
+        public string GetLastName()
+        {
+            return this._lastName;
+        }
+
+        public void SetLastName()
+        {
+            Console.WriteLine("Type the last name: ");
+            this._lastName = Console.ReadLine();
+        }
+
+
+        public string FullName()
+        {
+            Console.WriteLine(this._firstName + " " + this._lastName);
+            return (this._firstName + " " + this._lastName);
+        }
+    }
+}
---
+++ Section 5 Practice/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] people = new Person[3];
+
+            for (int x = 0; x < 3; x++)
+            {
+                people[x] = new Person();
+                people[x].SetFirstName();
+                people[x].SetLastName();
+            }
+
+            foreach (Person callItAnthing in people)
+            {
+                callItAnthing.FullName();
+            }
+
+            
+        }
+
+    }
+}
