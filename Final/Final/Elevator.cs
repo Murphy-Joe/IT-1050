24468d2ce096b57627829d8a21e180806cf4be48
Murphy-Joe <ibidnsell@yahoo.com>
2020-05-04 16:42:34 -0400

    send to github

---
+++ Final/Final/Elevator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Final
+{
+    class Elevator
+    {
+        double MaxWeight;
+        Passenger[] Occupants;
+
+        public Elevator(int maxOccupants, double maxWeight)
+        {
+            Occupants = new Passenger[maxOccupants];
+            MaxWeight = maxWeight;
+        }
+
+        public void AddOccupant(Passenger passenger, int index)
+        {
+            Occupants[index] = passenger;
+        }
+
+        public double GetCurrentWeight()
+        {
+            double totalWeight = 0;
+
+            foreach (Passenger whoever in Occupants)
+            {
+                totalWeight += whoever.GetWeight();
+            }
+
+            return totalWeight;
+        }
+
+        public bool IsOverMaxCapacity()
+        {
+            return GetCurrentWeight() > MaxWeight;
+        }
+    }
+
+
+}
---
+++ Final/Final/Passenger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Final
+{
+    class Passenger
+    {
+        string Name;
+        double Weight;
+
+        public Passenger(string name, double weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+
+        public string GetName()
+        {
+            return this.Name;
+        }
+
+        public double GetWeight()
+        {
+            return this.Weight;
+        }
+    }
+}
